14a363555668b0844e8619bd6755ff3019891d2f
Paul Stupka <paul.stupka@sitecore.com>
2019-09-13 15:28:47 +0300

    empty project

---
+++ src/Sitecore.Support.353141/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.353141")]
+[assembly: AssemblyProduct("Sitecore.Support.353141")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Cumulative patch for SitecoreItemCrawler issues")]
+
