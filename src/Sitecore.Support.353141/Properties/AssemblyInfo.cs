8b484f4f5016ec2c16739cc2baa3d37c90b66766
Paul Stupka <paul.stupka@sitecore.com>
2019-09-13 16:22:03 +0300

    fix an issue with solution and project name

--- src/Sitecore.Support.353141/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.353141/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.353141")]
-[assembly: AssemblyProduct("Sitecore.Support.353141")]
+[assembly: AssemblyTitle("Sitecore.Support.358248")]
+[assembly: AssemblyProduct("Sitecore.Support.358248")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyDescription("Cumulative patch for SitecoreItemCrawler issues")]
 
